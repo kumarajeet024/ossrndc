993356cccef4860cf4be579e48a147cefb80b784
Ajeet <40895624+kumarajeet024@users.noreply.github.com>
2019-01-08 18:31:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -672,7 +672,13 @@
     </footer>
 
 
-
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-2010343548885514",
+    enable_page_level_ads: true
+  });
+</script>
 
 
   <script src="js/jquery.min.js"></script>
